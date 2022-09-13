704688381b0cac9ff2215d9f89fdc8705f4be806
imansaeedjama <imansaeedjama@gmail.com>
2022-09-13 03:12:22 -0700

    task 7 commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabet in reverse
+ *
+ * Description: using the main function
+ * This program prints the lowercase alphabet in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
