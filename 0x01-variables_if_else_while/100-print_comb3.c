3fcfcb4ef043946959a39eca1620168a6fffbb4c
imansaeedjama <imansaeedjama@gmail.com>
2022-09-13 03:41:28 -0700

    task 10 commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all possible different combinations of two digits
+ *
+ * Description: using main function
+ * This program prints all possible different combinations of two digits
+ * Return: 0
+ */
+int main(void)
+{
+	int n1 = 0, n2;
+
+	while(n <= 9)
+	{
+		if(n1 != n2 && n1 < n2)
+		{
+			putchar(n1 + 48);
+			putchar(n2 + 48);
+
+			if(n1 + n2 != 17)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		++n2
+	}
+	++n1
+		putchar('\n');
+	return (0);
+}
