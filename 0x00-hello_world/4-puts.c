604ebe1f6de80fada2259ee8c4aa1d023b6b4049
imansaeedjama <imansaeedjama@gmail.com>
2022-09-12 20:39:43 -0700

    task 4 update commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+/** 
+* main - prints a string using the put function
+*
+* Descreption: using main fucntion
+* function put inside main prints "Programming is like building a multilingual puzzle
+* Returns: 0
+* */
+
+
+
+
+
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-return:0;
+	return (0);
 }
