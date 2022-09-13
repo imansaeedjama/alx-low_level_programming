f047751561219dd54e84fb1520f4be9a02dffbfc
imansaeedjama <imansaeedjama@gmail.com>
2022-09-12 20:46:21 -0700

    task 4 update 2 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,18 +2,10 @@
 
 /** 
 * main - prints a string using the put function
-*
-* Descreption: using main fucntion
-* function put inside main prints "Programming is like building a multilingual puzzle
-* Returns: 0
-* */
-
-
-
-
-
-
-
+* Description: using the main function
+* This program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
