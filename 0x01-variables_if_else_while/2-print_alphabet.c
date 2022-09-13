dbff36c66654950146559b1a26e644e76a17295d
imansaeedjama <imansaeedjama@gmail.com>
2022-09-13 01:40:47 -0700

    task 2 commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lower case
+ *
+ * Description: using the main function
+ * This program prints the alphabet in lower case
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
