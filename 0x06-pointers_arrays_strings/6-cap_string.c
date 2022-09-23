fb5b7b69943d284f76ab2e81e12baf096a531055
imansaeedjama <imansaeedjama@gmail.com>
2022-09-23 12:54:29 -0700

    task 6 fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,7 @@ char *cap_string(char *str)
 {
 	int index = 0;
 
-	while (str[index])
+	while (str[++index])
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
@@ -28,7 +28,6 @@ char *cap_string(char *str)
 				str[index - 1] == '}' ||
 				 index == 0)
 			str[index] -= 32;
-		index++;
 	}
 	return (str);
 }
