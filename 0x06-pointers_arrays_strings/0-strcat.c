f250b74e0eeb34603136fee551402b904e9154c4
imansaeedjama <imansaeedjama@gmail.com>
2022-09-23 15:55:38 -0700

    task 0 commit fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,15 +10,11 @@
  */
 char *_strcat(char *dest, const char *src)
 {
-	int len = 0, i;
+	int index = 0, dest_len = 0;
 
-	while (dest[len])
-		len++;
-	for (i = 0; src[i] != 0; i++)
-	{
-		dest[len] = src[i];
-		len += 1;
-	}
-	dest[len] = '\0';
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
 }
