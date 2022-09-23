e8a983cc30b435a74550df40472bb13b6892023f
imansaeedjama <imansaeedjama@gmail.com>
2022-09-23 16:20:10 -0700

    task 6 commit fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,19 +1,34 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalizes all words of a string
- * @str: The string to be capitalized
- * Return: A pointer to the changed string
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ * Return: A pointer to the changed string.
  */
-char *string_toupper(char *s)
+char *cap_string(char *str)
 {
-	int count = 0;
+	int index = 0;
 
-	while (*(s + count) != '\0')
+	while (str[index])
 	{
-		if ((*(s + count) >= 97) && (*(s + count) <= 122))
-			*(s + count) = *(s + count) - 32;
-		count++;
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+		if (str[index - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}' ||
+				index == 0)
+			str[index] -= 32;
+		index++;
 	}
-	return (s)
+	return (str);
 }
