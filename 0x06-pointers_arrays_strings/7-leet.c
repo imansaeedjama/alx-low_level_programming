2fdb1f5c30f917f4ba30553cec263dc8c10308b1
imansaeedjama <imansaeedjama@gmail.com>
2022-09-23 16:25:42 -0700

    task 7 commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * leet - Encodes a string to 1337
- * @str: The string to be encoded
- * Return: A pointer to the encoded string
+ * leet - Encodes a string to 1337.
+ * @str: The string to be encoded.
+ * Return: A pointer to the encoded string.
  */
 char *leet(char *str)
 {
